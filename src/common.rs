53abe98613bf167db8397bd487cbfc7e3ecc8554
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-01 11:30:36 -0700

    (feat) Always sort by priority.

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,4 @@
-pub fn sorter<S: Str>(&(ref set_a, priority_a): &(S, f64),
-                      &(ref set_b, priority_b): &(S, f64)) -> Ordering {
-    // If these are the same charset, sort by priority
-    if set_a.as_slice() == set_b.as_slice() {
-        priority_a.partial_cmp(&priority_b).unwrap()
-    } else {
-        set_a.as_slice().cmp(&set_b.as_slice())
-    }
+pub fn sorter<S>(&(_, priority_a): &(S, f64),
+                 &(_, priority_b): &(S, f64)) -> Ordering {
+    priority_a.partial_cmp(&priority_b).unwrap()
 }
