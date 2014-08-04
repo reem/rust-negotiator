e7d713fe7bae609e0b8b6da5bdfafb6594686605
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-04 12:17:09 -0700

    (fix) Do a revsort by priority.

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,5 @@
 pub fn sorter<S>(&(_, priority_a): &(S, f64),
                  &(_, priority_b): &(S, f64)) -> Ordering {
-    priority_a.partial_cmp(&priority_b).unwrap()
+    // Revsort
+    priority_b.partial_cmp(&priority_a).unwrap()
 }
